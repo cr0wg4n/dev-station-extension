2941d776e399f9c3fdc15f1ed77adfa63bd3499d
cr0wg4n <mcm.crw@gmail.com>
2023-03-03 17:32:04 -0400

    feature: base

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+  return (
+    <Navbar title="Dev Station" />
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Options from "./Options";
+import logo from '../../../public/icon.png'
+
+export interface INavbarProps {
+  title: string;
+}
+
+const Navbar: React.FC<INavbarProps> = ({ title }:INavbarProps) => {
+  return <div className="w-full p-2 text-white bg-black">
+    <div className="flex justify-between items-center">
+      <div className="flex justify-center items-center">
+        <img src={logo} alt="logo" className="max-h-7 mr-2"/>
+        <div className="text-base">
+          {title}
+        </div>
+      </div>
+      <div>
+        <Options />
+      </div>
+    </div>
+  </div>
+}
+
+export default Navbar
