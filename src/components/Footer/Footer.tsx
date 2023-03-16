2284a1a855389f3c91f6c88fdaca5a00d217e7d7
cr0wg4n <mcm.crw@gmail.com>
2023-03-16 17:28:06 -0400

    doc: readme

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Footer: React.FC = () => {
-  return <div className="p-3 text-right text-xs">
+  return <div className="px-3 pb-3 text-right text-xs">
     Made with ❤️ by <a 
       href="https://github.com/cr0wg4n" 
       target="_blank" 
