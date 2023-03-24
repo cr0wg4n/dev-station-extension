95b477958789daeb54da1a139d6068a2fffb3089
cr0wg4n <mcm.crw@gmail.com>
2023-03-24 08:54:43 -0400

    fix: improving vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+const root = resolve(__dirname, 'src')
+const dist = resolve(__dirname, 'dist') 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: dist,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        background: resolve(root, 'background.ts')
+      }
+    }
+  }
 })
