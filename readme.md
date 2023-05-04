e4330b9aa3a892bddbad4a829f4ecaf88064b9d4
cr0wg4n <mcm.crw@gmail.com>
2023-05-04 11:05:19 -0400

    feature: new tool in development

--- readme.md
+++ readme.md
@@ -12,9 +12,9 @@ npm run dev
 The funcionality behind the frontend is hosted at [dev-station-core](https://github.com/cr0wg4n/dev-station-core)
 
 ### Features in development (easy to hard)
-- [x] CSS outline for debugging (done!)
-- [x] Quotes and Mantras (done!), Thank you so much https://quotable.io
-- [ ] RSS checker
+- [x] CSS outline for debugging
+- [x] Quotes and Mantras, Thank you so much https://quotable.io
+- [ ] RSS checker (On development)
 - [ ] Image Downloader
 - [ ] Url environment
 - [ ] Lorem Ipsum Generator
--- src/store/toolbar/tools.tsx
+++ src/store/toolbar/tools.tsx
@@ -1,7 +1,7 @@
 import CssOutlineDebugger from "../../components/Tools/CssOutlineDebugger"
 import { ToolComponent, Tools } from "../../core/tools"
 import { Tool } from "./types"
-import { FaCss3Alt } from 'react-icons/fa'
+import { FaCss3Alt, FaRssSquare } from 'react-icons/fa'
 
 const toolComponents: ToolComponent[] = [
   {
@@ -9,6 +9,11 @@ const toolComponents: ToolComponent[] = [
     rootComponent: <CssOutlineDebugger />,
     icon: <FaCss3Alt size={16} className="mr-1 text-white"/>
   },
+  {
+    name: Tools.RSS_CHECKER,
+    rootComponent: <></>,
+    icon: <FaRssSquare size={16} className="mr-1 text-white"/>
+  }
 ]
 
 export function findTool(name: string): ToolComponent | undefined{
@@ -22,6 +27,12 @@ export const tools: Tool[] = [
     id: Tools.CSS_OUTLINE,
     title: 'CSS Debugger',
     active: false,
-    description: 'This is a css debugger tool'
+    description: 'Enable and disable css outline globally'
+  },
+  {
+    id: Tools.RSS_CHECKER,
+    title: 'RSS Checker',
+    active: true,
+    description: ''
   }
 ]
